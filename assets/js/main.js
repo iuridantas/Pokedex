6c417ada0a1a211c1fcca2c2ff77216732a3af32
Iuri Dantas <uri.dantas@hotmail.com>
2022-07-22 21:39:22 -0300

    atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,11 @@ const fetchpokemon = () => {
       const lispokemons = pokemons.reduce((accumulator, pokemon) =>{
         const types = pokemon.types.map(typeinfo => typeinfo.type.name)
 
+        console.log(pokemon)
 
         accumulator += `
         <li class="card ${types[0]}">
-        <img class = "card-image" alt="${pokemon.name}" src="${pokemon["official-artwork"]}.png"/>
+        <img class = "card-image" alt="${pokemon.name}" src="${pokemon.sprites.other["dream_world"].front_default}"/>
           <p class = "card-subtitle"><span>Nº </span>${pokemon.id.toString().padStart(3, '0')}</p>
           <h2 class = "card-title">${pokemon.name}</h2>
           <h3 class = "card-subtitle">${types.join(` | `)}</h3>
